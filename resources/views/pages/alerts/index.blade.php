2ff6c6ad22ee329cff8ec4355bd1c896a97dece3
microceptedutech <microcepttechnologies@gmail.com>
2020-03-02 23:40:37 +0530

    bannner and traditional alerts added

--- resources/views/pages/alerts/index.blade.php
+++ resources/views/pages/alerts/index.blade.php
@@ -2,4 +2,6 @@
 @section('content')
 @include('pages.alerts.left_accent')
 @include('pages.alerts.top_accent')
+@include('pages.alerts.banner')
+@include('pages.alerts.traditional')
 @endsection
---
+++ resources/views/pages/alerts/traditional.blade.php
@@ -0,0 +1,58 @@
+<div class="grid grid-rows-12 bg-white shadow-sm w-full p-6">
+    <div class="row-span-3 mt-2">
+       <h5 class="text-lg text-gray-500 mb-2">Traditional</h5>
+        <div class="bg-red-100 border-t border-r border-b border-l border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Holy smokes!</strong>
+            <span class="block sm:inline">Something seriously bad happened.</span>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            </span>
+        </div>
+    </div>
+    <div class="row-span-3 mt-2">
+        <div class="bg-indigo-100 border-t border-r border-b border-l border-indigo-400 text-indigo-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Information !</strong>
+            <span class="block sm:inline">Know this information.</span>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="fill-current h-6 w-6 text-indigo-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            </span>
+        </div>
+    </div>
+    <div class="row-span-3 mt-2">
+       <div class="bg-orange-100 border-t border-r border-b border-l border-orange-400 text-orange-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Warning !</strong>
+            <span class="block sm:inline">You should be warned about this.</span>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="fill-current h-6 w-6 text-orange-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            </span>
+        </div>
+    </div>
+    <div class="row-span-3 mt-2">
+        <div class="bg-pink-100 border-t border-r border-b border-l border-pink-400 text-pink-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Warning !</strong>
+            <span class="block sm:inline">You should be warned about this.</span>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="fill-current h-6 w-6 text-pink-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            </span>
+        </div>
+    </div>
+    <div class="row-span-3 mt-2">
+       <div class="bg-green-100 border-t border-r border-b border-l border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Success !</strong>
+            <span class="block sm:inline">You have successfully posted.</span>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            </span>
+        </div>
+        
+    </div>
+    <div class="row-span-3 mt-2">
+        <div class="bg-purple-100 border-t border-r border-b border-l border-purple-400 text-purple-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Success !</strong>
+            <span class="block sm:inline">You have successfully posted.</span>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="fill-current h-6 w-6 text-purple-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            </span>
+        </div>
+    </div>
+</div>
